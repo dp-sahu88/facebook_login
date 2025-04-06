304ce5e39c0c4b777c9836c467bcd1726e3e5d70
Deviprasad Sahu <deviprasadsahu88@gmail.com>
2025-04-06 22:09:05 +0530

    console log the user access token

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-window.fbAsyncInit = function() {
-  FB.init({
-    appId            : '184538382745930',
-    autoLogAppEvents : true,
-    xfbml            : true,
-    version          : 'v3.2'
-  });
-  FB.getLoginStatus(function(response) {
-    if (response.status === 'connected') {
-      FB.api('/me', {
-        fields: 'id,name,email,picture'
-      }, function(response) {
-        console.log(`Logged in user: ${response.name}`);
-        const profilePic = document.createElement('img');
-        profilePic.src = response.picture.data.url;
-        document.body.appendChild(profilePic);
-      });
-    }
-  });
-};
+document.querySelector('.fb-login-button').addEventListener('click', () => {
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '184538382745930',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v3.2'
+    });
+    FB.getLoginStatus(function(response) {
+      if (response.status === 'connected') {
+        console.log('User is logged in.');
+        const accessToken = response.authResponse.accessToken;
+        console.log(`Access Token: ${accessToken}`);
+        FB.api('/me', {
+          fields: 'id,name,email,picture'
+        }, function(response) {
+          console.log(`Logged in user: ${response.name}`);
+          const profilePic = document.createElement('img');
+          profilePic.src = response.picture.data.url;
+          document.body.appendChild(profilePic);
+        });
+      } else {
+        console.log('User is not logged in.');
+      }
+    });
+  };
+});
 
