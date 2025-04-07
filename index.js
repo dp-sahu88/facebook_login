afe62f7a1eb99de4340da1418de00652a6918e52
Deviprasad Sahu <deviprasadsahu88@gmail.com>
2025-04-07 21:00:45 +0530

    added a button to show credentials button

--- index.js
+++ index.js
@@ -4,15 +4,25 @@
       if (response.status === 'connected') {
         console.log('User is logged in.');
         const accessToken = response.authResponse.accessToken;
-        console.log(`Access Token: ${accessToken}`);
+        const responseDiv = document.createElement('div');
+        responseDiv.innerHTML = `
+          <p>Access Token: ${accessToken}</p>
+        `;
+        document.body.appendChild(responseDiv);
+
         FB.api('/me', {
           fields: 'id,name,email,picture'
         }, function(response) {
-          console.log(`Logged in user: ${response.name}`);
           const profilePic = document.createElement('img');
           profilePic.src = response.picture.data.url;
+          document.body.appendChild(document.createElement('br'));
+          document.body.appendChild(document.createElement('br'));
           document.body.appendChild(profilePic);
+          const profileInfo = document.createElement('div');
+          profileInfo.innerHTML = `Name: ${response.name}<br>ID: ${response.id}<br>Email: ${response.email}`;
+          document.body.appendChild(profileInfo);
         });
+
       } else {
         console.log('User is not logged in.');
       }
